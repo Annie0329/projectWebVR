a63744464545eefc28f640e14821cb1c8345d80c
Annie0329 <0329annie@gmail.com>
2025-05-22 23:33:59 +0800

    0522

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ AFRAME.registerComponent('set-sky-on-click', {
     init: function () {
         const sky = document.querySelector('#image-360');
         const textPlane = document.querySelector('#textPlane');
-
         this.el.addEventListener('click', () => {
             const src = this.el.getAttribute('data-src');
             const rotation = this.el.getAttribute('data-rotation') || '0 0 0';
             const textPic = this.el.getAttribute('data-text');
+            // Set image and rotation on sky
             sky.setAttribute('material', 'src', src);
             sky.setAttribute('rotation', rotation);
             textPlane.setAttribute('src', textPic);
+            console.log(src)
         });
     }
 });
